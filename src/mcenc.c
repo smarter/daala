cc433292178bede3ea8ee13f411a384417cc9cfe
Guillaume Martres <smarter@ubuntu.com>
2014-08-28 03:27:55 +0200

    no subpel refine

--- src/mcenc.c
+++ src/mcenc.c
@@ -3971,6 +3971,7 @@ void od_mv_subpel_refine(od_mv_est_ctx *est, int ref, int cost_thresh) {
     We could also try rounding the results after refinement, I guess.
     I'm not sure it makes much difference*/
   od_mv_est_update_fullpel_mvs(est, ref);
+#if 0
   do {
     dcost = od_mv_est_refine(est, ref, 2, 2,
      OD_DIAMOND_NSITES, OD_DIAMOND_SITES);
@@ -4007,6 +4008,7 @@ void od_mv_subpel_refine(od_mv_est_ctx *est, int ref, int cost_thresh) {
     }
   }
   state->mv_res = best_mv_res;
+#endif
 }
 
 void od_mv_est(od_mv_est_ctx *est, int ref, int lambda) {
