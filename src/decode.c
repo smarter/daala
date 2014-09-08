smarter/daala

commit dd3fca176f087d8fe72ddf2e972d728ef92d941f

Optimize level 3 motion flags in the margin.

src/decode.c
@@ -629,11 +629,46 @@ static void od_dec_mv_unpack(daala_dec_ctx *dec) {
     }
   }
   /*Level 3.*/
+  /*Level 3 motion vector flags are complicated on the edges. See the comments
+    in encode.c for why this code is complicated.*/
   for (vy = 1; vy <= nvmvbs; vy += 2) {
     for (vx = 1; vx <= nhmvbs; vx += 2) {
       mvp = &grid[vy][vx];
-      if (grid[vy-1][vx-1].valid && grid[vy-1][vx+1].valid
-       && grid[vy+1][vx+1].valid && grid[vy+1][vx-1].valid) {
+      if (vy < 2 || vy > nvmvbs - 2) {
+        if ((vx == 3 && grid[vy == 1 ? vy - 1 : vy + 1][vx - 1].valid)
+         || (vx == nhmvbs - 3
+         && grid[vy == 1 ? vy - 1 : vy + 1][vx + 1].valid)) {
+          mvp->valid = 1;
+          /*MV is outside frame and will be zero.*/
+        }
+        else if (vx > 3 && vx < nhmvbs - 3) {
+          if (!(vx & 2) && grid[vx == 3 ? vy - 1 : vy + 1][vx + 1].valid) {
+            /*0 = both valid, 1 = only this one, 2 = other one valid*/
+            int s;
+            s = od_ec_decode_cdf_q15(&dec->ec, OD_UNIFORM_CDF_Q15(3), 3);
+            mvp->valid = s != 2;
+            grid[vy][vx + 2].valid = s != 1;
+            /*MV is outside frame and will be zero.*/
+          }
+        }
+      }
+      else if (vx < 2 || vx > nhmvbs - 2) {
+        if ((vy == 3 && grid[vy - 1][vx == 1 ? vx - 1 : vx + 1].valid)
+         || (vy == nvmvbs - 3
+         && grid[vy - 1][vx == 1 ? vx - 1 : vx + 1].valid)) {
+          mvp->valid = 1;
+          /*MV is outside frame and will be zero.*/
+        }
+        else if (!(vy & 2) && grid[vy + 1][vx == 1 ? vx - 1 : vx + 1].valid) {
+          int s;
+          s = od_ec_decode_cdf_q15(&dec->ec, OD_UNIFORM_CDF_Q15(3), 3);
+          mvp->valid = s != 2;
+          grid[vy + 2][vx].valid = s != 1;
+          /*MVs are valid but will be zero.*/
+        }
+      }
+      else if (grid[vy - 1][vx - 1].valid && grid[vy - 1][vx + 1].valid
+       && grid[vy + 1][vx + 1].valid && grid[vy + 1][vx - 1].valid) {
         mvp->valid = od_ec_decode_bool_q15(&dec->ec, 16384);
         if (mvp->valid) {
           od_decode_mv(dec, mvp, vx, vy, 3, mv_res, width, height);

src/encode.c
@@ -1202,19 +1202,64 @@ int daala_encode_img_in(daala_enc_ctx *enc, od_img *img, int duration) {
       }
     }
     /*Level 3.*/
+    /*Level 3 motion vector flags outside the frame are specially coded
+      since more information is known. On the grid edge, an L2 MV will only be
+      valid if a L3 MV is needed outside of the frame. In the middle of the
+      edge, this implies a tristate of the two possible child L3 MVs; they
+      can't both be invalid. At the corner, one of the child L3 vectors will
+      never appear, so an L2 MV directly implies the remaining L3 child.*/
     for (vy = 1; vy <= nvmvbs; vy += 2) {
       for (vx = 1; vx <= nhmvbs; vx += 2) {
         mvp = &grid[vy][vx];
-        if (grid[vy-1][vx-1].valid && grid[vy-1][vx+1].valid
-         && grid[vy+1][vx+1].valid && grid[vy+1][vx-1].valid) {
-          od_ec_encode_bool_q15(&enc->ec, mvp->valid, 16384);
-          if (mvp->valid && vx >= 2 && vy >= 2 && vx <= nhmvbs - 2 &&
-           vy <= nvmvbs - 2) {
-            od_encode_mv(enc, mvp, vx, vy, 3, mv_res, width, height);
+        if (vy < 2 || vy > nvmvbs - 2) {
+          if ((vx == 3 && grid[vy == 1 ? vy - 1 : vy + 1][vx - 1].valid)
+           || (vx == nhmvbs - 3
+           && grid[vy == 1 ? vy - 1 : vy + 1][vx + 1].valid)) {
+            /*MVs are valid but will be zero.*/
+          }
+          else if (vx > 3 && vx < nhmvbs - 3) {
+            if (!(vx & 2) && grid[vx == 3 ? vy - 1 : vy + 1][vx + 1].valid) {
+              /*0 = both valid, 1 = only this one, 2 = other one valid*/
+              int s;
+              s = mvp->valid && grid[vy][vx + 2].valid ? 0 : mvp->valid
+               + (grid[vy][vx + 2].valid << 1);
+              od_ec_encode_cdf_q15(&enc->ec, s, OD_UNIFORM_CDF_Q15(3), 3);
+              /*MVs are valid but will be zero.*/
+            }
+            else if (!(vx & 2)) {
+              OD_ASSERT(!mvp->valid && !grid[vy][vx + 2].valid);
+            }
           }
           else {
             OD_ASSERT(!mvp->valid);
           }
+        }
+        else if (vx < 2 || vx > nhmvbs - 2) {
+          if ((vy == 3 && grid[vy - 1][vx == 1 ? vx - 1 : vx + 1].valid)
+           || (vy == nvmvbs - 3
+           && grid[vy - 1][vx == 1 ? vx - 1 : vx + 1].valid)) {
+            /*MVs are valid but will be zero.*/
+          }
+          else if (!(vy & 2) && grid[vy + 1][vx == 1 ? vx - 1 : vx + 1].valid) {
+            int s;
+            s = mvp->valid && grid[vy + 2][vx].valid ? 0 : mvp->valid
+             + (grid[vy + 2][vx].valid << 1);
+            od_ec_encode_cdf_q15(&enc->ec, s, OD_UNIFORM_CDF_Q15(3), 3);
+            /*MVs are valid but will be zero.*/
+          }
+          else if (!(vy & 2)) {
+            OD_ASSERT(!mvp->valid && !grid[vy + 2][vx].valid);
+          }
+        }
+        else if (grid[vy - 1][vx - 1].valid && grid[vy - 1][vx + 1].valid
+         && grid[vy + 1][vx + 1].valid && grid[vy + 1][vx - 1].valid) {
+          od_ec_encode_bool_q15(&enc->ec, mvp->valid, 16384);
+          if (mvp->valid) {
+            od_encode_mv(enc, mvp, vx, vy, 3, mv_res, width, height);
+          }
+        }
+        else {
+          OD_ASSERT(!mvp->valid);
         }
       }
     }

commit b0093639bf3a192c3534f702e5f8b9f70ee884a2

Fix another desync caused by dd3fca176f087d8fe72ddf2e972d728ef92d941f
The decoder assumed that mv_grid was cleared at every frame which was
not the case until now. Clear it both in the decoder and in the encoder
to avoid this kind of bugs in the future.

src/decode.c
@@ -754,6 +754,7 @@ int daala_decode_packet_in(daala_dec_ctx *dec, od_img *img,
   od_adapt_ctx_reset(&dec->state.adapt, mbctx.is_keyframe);
   od_decode_block_sizes(dec);
   if (!mbctx.is_keyframe) {
+    od_state_mvs_reset(&dec->state);
     od_dec_mv_unpack(dec);
     od_state_mc_predict(&dec->state, OD_FRAME_PREV);
   }

src/encode.c
@@ -1040,6 +1040,7 @@ int daala_encode_img_in(daala_enc_ctx *enc, od_img *img, int duration) {
 #endif
     OD_LOG((OD_LOG_ENCODER, OD_LOG_INFO, "Predicting frame %i:",
      (int)daala_granule_basetime(enc, enc->state.cur_time)));
+    od_state_mvs_reset(&enc->state);
     /*2851196 ~= sqrt(ln(2)/6) in Q23.
       The lower bound of 56 is there because we do not yet consider PVQ noref
        flags during the motion search, so we waste far too many bits trying to

src/internal.c
@@ -191,6 +191,18 @@ void **od_calloc_2d(size_t _height, size_t _width, size_t _sz) {
   return (void **)ret;
 }
 
+void od_clear_2d(void *ptr, size_t height, size_t width, size_t sz) {
+  size_t colsz;
+  size_t rowsz;
+  size_t datsz;
+  char *datptr;
+  colsz = height*sizeof(void *);
+  rowsz = sz*width;
+  datsz = rowsz*height;
+  datptr = (char*)ptr + colsz;
+  memset(datptr, 0, datsz);
+}
+
 void od_free_2d(void *_ptr) {
   _ogg_free(_ptr);
 }

src/internal.h
@@ -154,6 +154,7 @@ ptrdiff_t oggbyte_bytes_left(oggbyte_buffer *_b);
 int od_ilog(ogg_uint32_t _v);
 void **od_malloc_2d(size_t _height, size_t _width, size_t _sz);
 void **od_calloc_2d(size_t _height, size_t _width, size_t _sz);
+void od_clear_2d(void *ptr, size_t height, size_t width, size_t sz);
 void od_free_2d(void *_ptr);
 
 # define OD_DIVU_DMAX (64)

src/state.c
@@ -177,12 +177,21 @@ static int od_state_mvs_init(od_state *state) {
   int nvmvbs;
   nhmvbs = (state->nhmbs + 1) << 2;
   nvmvbs = (state->nvmbs + 1) << 2;
-  state->mv_grid = (od_mv_grid_pt **)od_calloc_2d(nvmvbs + 1, nhmvbs + 1,
+  state->mv_grid = (od_mv_grid_pt **)od_malloc_2d(nvmvbs + 1, nhmvbs + 1,
    sizeof(**state->mv_grid));
   if (OD_UNLIKELY(!state->mv_grid)) {
     return OD_EFAULT;
   }
   return OD_SUCCESS;
+}
+
+void od_state_mvs_reset(od_state *state) {
+  int nhmvbs;
+  int nvmvbs;
+  nhmvbs = (state->nhmbs + 1) << 2;
+  nvmvbs = (state->nvmbs + 1) << 2;
+  od_clear_2d(state->mv_grid, nvmvbs + 1, nhmvbs + 1,
+   sizeof(**state->mv_grid));
 }
 
 static void od_restore_fpu_c(void) {}

src/state.h
@@ -218,6 +218,7 @@ struct od_state{
 int od_state_init(od_state *_state, const daala_info *_info);
 void od_state_clear(od_state *_state);
 
+void od_state_mvs_reset(od_state *state);
 void od_adapt_ctx_reset(od_adapt_ctx *state, int is_keyframe);
 void od_state_set_mv_res(od_state *state, int mv_res);
 void od_state_pred_block_from_setup(od_state *_state, unsigned char *_buf,