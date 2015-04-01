4eb628294629f1596aaee5f4605de2f0fa46ff31
Jean-Marc Valin <jmvalin@jmvalin.ca>
2015-04-01 19:33:39 -0400

    larger lambda

--- src/encode.c
+++ src/encode.c
@@ -1100,7 +1100,7 @@ static void od_predict_frame(daala_enc_ctx *enc) {
     predict unpredictable areas when lamba is too small.
    Hopefully when we fix that, we can remove the limit.*/
   od_mv_est(enc->mvest, OD_FRAME_PREV,
-   OD_MAXI((2851196 + (((1 << OD_COEFF_SHIFT) - 1) >> 1) >> OD_COEFF_SHIFT)*
+   OD_MAXI((3851196 + (((1 << OD_COEFF_SHIFT) - 1) >> 1) >> OD_COEFF_SHIFT)*
    enc->quantizer[0] >> (23 - OD_LAMBDA_SCALE), 56));
   od_state_mc_predict(&enc->state, OD_FRAME_PREV);
   /*Do edge extension here because the block-size analysis needs to read
