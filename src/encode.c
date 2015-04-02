f87426c11ac75cbce7b6ab98025026bd308fa0a2
Jean-Marc Valin <jmvalin@jmvalin.ca>
2015-04-01 22:44:24 -0400

    lambda 1.4 and lower floor

--- src/encode.c
+++ src/encode.c
@@ -1100,8 +1100,8 @@ static void od_predict_frame(daala_enc_ctx *enc) {
     predict unpredictable areas when lamba is too small.
    Hopefully when we fix that, we can remove the limit.*/
   od_mv_est(enc->mvest, OD_FRAME_PREV,
-   OD_MAXI((3563995 + (((1 << OD_COEFF_SHIFT) - 1) >> 1) >> OD_COEFF_SHIFT)*
-   enc->quantizer[0] >> (23 - OD_LAMBDA_SCALE), 56));
+   OD_MAXI((4000000 + (((1 << OD_COEFF_SHIFT) - 1) >> 1) >> OD_COEFF_SHIFT)*
+   enc->quantizer[0] >> (23 - OD_LAMBDA_SCALE), 40));
   od_state_mc_predict(&enc->state, OD_FRAME_PREV);
   /*Do edge extension here because the block-size analysis needs to read
     outside the frame, but otherwise isn't read from.*/
