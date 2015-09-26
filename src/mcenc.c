a2dfa3089db6fe635c155a31aca522c187fd66b3
Guillaume Martres <smarter@ubuntu.com>
2015-09-26 19:36:50 +0200

    Same acceleration predictors as fullpel BMA search

--- src/mcenc.c
+++ src/mcenc.c
@@ -2736,11 +2736,11 @@ static void od_mv_est_init_mv(od_mv_est_ctx *est, int ref, int vx, int vy,
       }
       /*The constant acceleration predictor:*/
       cands[4][0] = OD_CLAMPI(mvxmin,
-       OD_DIV_ROUND_POW2(mv->bma_mvs[1][ref][0]*est->mvapw[ref][0]
-       - mv->bma_mvs[2][ref][0]*est->mvapw[ref][1], 16, 0x8000), mvxmax);
+       2*OD_DIV_ROUND_POW2((mv->bma_mvs[1][ref][0] >> 1)*est->mvapw[ref][0]
+        - (mv->bma_mvs[2][ref][0] >> 1)*est->mvapw[ref][1], 16, 0x8000), mvxmax);
       cands[4][1] = OD_CLAMPI(mvymin,
-       OD_DIV_ROUND_POW2(mv->bma_mvs[1][ref][1]*est->mvapw[ref][0]
-       - mv->bma_mvs[2][ref][1]*est->mvapw[ref][1], 16, 0x8000), mvymax);
+       2*OD_DIV_ROUND_POW2((mv->bma_mvs[1][ref][1] >> 1)*est->mvapw[ref][0]
+        - (mv->bma_mvs[2][ref][1] >> 1)*est->mvapw[ref][1], 16, 0x8000), mvymax);
       /*Examine the candidates in Set C.*/
       for (ci = 0; ci < 5; ci++) {
         candx = cands[ci][0];
