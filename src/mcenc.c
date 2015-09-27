e4778b21969e241f071aafcb86ca00498d71458d
Guillaume Martres <smarter@ubuntu.com>
2015-09-27 02:01:12 +0200

    Disable rounding before the BMA gradient descent

--- src/mcenc.c
+++ src/mcenc.c
@@ -2783,6 +2783,7 @@ static void od_mv_est_init_mv(od_mv_est_ctx *est, int ref, int vx, int vy,
         int site;
         int b;
         /*Round MV to fullpel before starting the search.*/
+        /*
         candx = (best_vec[0] >> 1) << 1;
         candy = (best_vec[1] >> 1) << 1;
         if (candx != best_vec[0] || candy != best_vec[1]) {
@@ -2793,6 +2794,7 @@ static void od_mv_est_init_mv(od_mv_est_ctx *est, int ref, int vx, int vy,
            candx, candy, pred[0], pred[1], ref, ref_pred);
           best_cost = (best_sad << OD_ERROR_SCALE) + best_rate*est->lambda;
         }
+        */
         /*Gradient descent pattern search.*/
         mvstate = 0;
         for (;;) {
