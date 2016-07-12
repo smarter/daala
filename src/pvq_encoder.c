170611edc8317332fb81bdd978878435ebd08608
Guillaume Martres <smarter@ubuntu.com>
2016-07-12 02:59:27 +0100

    Add actual cost of coding DC to DC rate

--- src/pvq_encoder.c
+++ src/pvq_encoder.c
@@ -864,6 +864,17 @@ int od_pvq_encode(daala_enc_ctx *enc,
     dc_rate = -OD_LOG2((double)(skip_cdf[3] - skip_cdf[2])/
      (double)(skip_cdf[2] - skip_cdf[1]));
     dc_rate += 1;
+    {
+      int tell2;
+      od_rollback_buffer dc_buf;
+      tell2 = od_ec_enc_tell_frac(&enc->ec);
+      od_encode_checkpoint(enc, &dc_buf);
+      generic_encode(&enc->ec, &enc->state.adapt.model_dc[pli],
+       0, -1, &enc->state.adapt.ex_dc[pli][bs][0], 2);
+      tell2 = od_ec_enc_tell_frac(&enc->ec) - tell2;
+      dc_rate += tell2/8.0;
+      od_encode_rollback(enc, &dc_buf);
+    }
     out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
      enc->pvq_norm_lambda);
   }
@@ -935,6 +946,17 @@ int od_pvq_encode(daala_enc_ctx *enc,
       dc_rate = -OD_LOG2((double)(skip_cdf[1] - skip_cdf[0])/
        (double)skip_cdf[0]);
       dc_rate += 1;
+      {
+        int tell2;
+        od_rollback_buffer dc_buf;
+        tell2 = od_ec_enc_tell_frac(&enc->ec);
+        od_encode_checkpoint(enc, &dc_buf);
+        generic_encode(&enc->ec, &enc->state.adapt.model_dc[pli],
+         0, -1, &enc->state.adapt.ex_dc[pli][bs][0], 2);
+        tell2 = od_ec_enc_tell_frac(&enc->ec) - tell2;
+        dc_rate += tell2/8.0;
+        od_encode_rollback(enc, &dc_buf);
+      }
       out[0] = od_rdo_quant(in[0] - ref[0], dc_quant, dc_rate,
        enc->pvq_norm_lambda);
     }
